c5a44fc14f11a7a44f1e57d5025df6b14ee23a14
alexioda <alexioda@gmail.com>
2026-03-29 21:18:21 -0400

    Wrap App component with CipherGate

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// main.tsx
+// Replace your existing main.tsx with this file.
+// The only change is wrapping <App /> with <CipherGate>.
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import CipherGate from './CipherGate';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CipherGate>
+      <App />
+    </CipherGate>
+  </React.StrictMode>
+);
